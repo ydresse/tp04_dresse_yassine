14a5549da4b9c1594fb3133f4a225da02e50e96e
Yassine <dresseyassine@gmail.com>
2020-11-01 21:58:55 +0100

    tp04

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { ProductComponent } from "./modules/product/product/product.component";
+const routes: Routes = [
+  {
+    path: "product",
+    component: ProductComponent
+  },
+  {
+    path: "home",
+    component: HomeComponent
+  },
+  {
+    path: "",
+    component: ProductComponent
+  },
+  {
+    path: "**",
+    component: ProductComponent
+  }
+];
+
+@NgModule({
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+import { AppComponent } from "./app.component";
+import { FooterComponent } from "./footer/footer.component";
+import { TetiereComponent } from "./tetiere/tetiere.component";
+import { ProductService } from "./product.service";
+import { AppRoutingModule } from "./app-routing.module";
+import { HomeComponent } from "./home/home.component";
+import { NgxsModule } from "@ngxs/store";
+import { ProductState } from "../store/states/products.state";
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgxsModule.forRoot([ProductState])
+  ],
+  declarations: [
+    AppComponent,
+    FooterComponent,
+    TetiereComponent,
+    HomeComponent
+  ],
+  bootstrap: [AppComponent],
+  providers: [ProductService]
+})
+export class AppModule {}
